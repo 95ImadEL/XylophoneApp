b9306e952ad306bcc221e17c6506a2b926bf72b3
Imad <imad.elbouazzaoui.ee@gmail.com>
2022-12-24 22:41:26 +0100

    Add playing multiple sounds

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,95 @@ class XylophoneApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-            child: Center(
-          child: TextButton(
-            onPressed: () {
-              final player = AudioPlayer();
-              player.play(AssetSource('note2.wav'));
-            },
-            child: Text("Click Me!"),
+          child: Column(
+            children: [
+              Expanded(
+                child: Container(
+                  color: Colors.red,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note1.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.orange,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note2.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.yellow,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note3.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.green,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note4.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.teal,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note5.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.blue,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note6.wav'));
+                    },
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.purple,
+                  child: TextButton(
+                    child: Container(),
+                    onPressed: () {
+                      final player = AudioPlayer();
+                      player.play(AssetSource('note7.wav'));
+                    },
+                  ),
+                ),
+              ),
+            ],
           ),
-        )),
+        ),
       ),
     );
   }
